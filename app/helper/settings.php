868a0b075016f39cf27858c3f2f921524a6eb1ed
davicheron2010 <davilvl12010@gmail.com>
2025-12-02 19:42:06 -0400

    a

--- app/helper/settings.php
+++ app/helper/settings.php
@@ -1,7 +1,12 @@
 <?php
-
+#Diretorio raiz da applicação WEB
 define('ROOT', dirname(__FILE__, 3));
-
-define ('EXT_VIEW', '.html');
-
+#Extensão padrão da camada de interação com usuário front-end.
+define('EXT_VIEW', '.html');
+#Diretorio do arquivos de template da view.
 define('DIR_VIEW', ROOT . '/app/view');
+
+#$_SERVER['HTTP_HOST'] : Indica o domínio (host) que foi chamado na URL pelo navegador. Domínio principal meusite.com ou localhost
+#$_SERVER['REQUEST_SCHEME'] : Indica o protocolo usado na requisição atual. podendo ser http ou https
+#Criamos uma constante chamada HOME que guarda automaticamente o endereço principal do site.
+define('HOME', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
