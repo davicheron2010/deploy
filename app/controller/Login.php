2d88a72786ab8485c743462b52f0f9ed7ae0ad04
davicheron2010 <davilvl12010@gmail.com>
2025-12-03 18:52:56 -0400

    fiz a página de login

---
+++ app/controller/Login.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\controller;
+
+use app\database\builder\InsertQuery;
+use app\database\builder\SelectQuery;
+
+class Login extends Base
+{
+    public function login($request, $response)
+    {
+        try {
+            $dadosTemplate = [
+                'titulo' => 'Autenticação'
+            ];
+            return $this->getTwig()
+                ->render($response, $this->setView('login'), $dadosTemplate)
+                ->withHeader('Content-Type', 'text/html')
+                ->withStatus(200);
+        } catch (\Exception $e) {
+        }
+    }
+        public function precadastro($request, $response)
+    {
+        try {
+            #Captura os dados do formulário
+            $form = $request->getParsedBody();
+            #Captura os dados do usuário
+            $dadosUsuario = [
+                'nome'              => $form['nome'],
+                'sobrenome'         => $form['sobrenome'],
+                'cpf'               => $form['cpf'],
+                'rg'                => $form['rg'],
+                'senha'             => password_hash($form['senha'], PASSWORD_DEFAULT)
+            ];
+            $IsInseted = InsertQuery::table('usuario')->save($dadosUsuario);
+            if (!$IsInseted){
+                return $this->SendJson(
+                    $response,
+                    ['status' => false, 'msg' => 'Restrição', $IsInseted, 'id' => 0],
+                    403
+                );
+            }
+            $id = SelectQuery::select('id')->from('usuario')-> order('id', 'desc')->fetch();
+            $id_usuario = $ $id['id'];
+        } catch (\Exception $e) {
+        }
+    }
+    public function autenticar($request, $response)
+    {
+        try {
+        } catch (\Exception $e) {
+        }
+    }
+}
--- app/route/route.php
+++ app/route/route.php
@@ -3,18 +3,25 @@
 use app\controller\Cliente;
 use app\controller\User;
 use app\controller\Home;
+use app\controller\Login;
 use Slim\Routing\RouteCollectorProxy;
 
 $app->get('/', Home::class . ':home');
 
 $app->get('/home', Home::class . ':home');
 
+$app->get('/login', Login::class . ':login');
+
+$app->group('/login', function (RouteCollectorProxy $group) {
+    $group->post('/precadastro', Login::class . ':precadastro');
+});
+
 $app->group('/usuario', function (RouteCollectorProxy $group) {
     $group->get('/lista', User::class . ':lista');
     $group->get('/cadastro', User::class . ':cadastro');
     $group->post('/insert', User::class . ':insert');
     $group->post('/delete', User::class . ':delete');
-    $group->post(   '/listuser', User::class . ':listuser');
+    $group->post('/listuser', User::class . ':listuser');
 });
 $app->group('/cliente', function (RouteCollectorProxy $group) {
     $group->get('/lista', Cliente::class . ':lista');
