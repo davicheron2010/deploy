acd8d73a1b0e89a4e25e8efd39a0097116380c5f
davicheron2010 <davilvl12010@gmail.com>
2025-12-09 21:39:12 -0400

    quase tudo funcionando

--- app/route/route.php
+++ app/route/route.php
@@ -1,13 +1,17 @@
 <?php
 
-use app\controller\User;
-use app\controller\Home;
 use app\controller\Login;
+use app\controller\Fornecedor;
+use app\controller\User;
+use app\controller\Cliente;
+use app\controller\Empresa;
+use app\controller\Home;
 use app\middleware\Middleware;
 use Slim\Routing\RouteCollectorProxy;
 
-$app->get('/', Home::class . ':home')->add(Middleware::authentication());
-$app->get('/home', Home::class . ':home')->add(Middleware::authentication());
+$app->get('/', Home::class . ':home');
+
+$app->get('/home', Home::class . ':home');
 $app->get('/login', Login::class . ':login');
 
 $app->group('/login', function (RouteCollectorProxy $group) {
@@ -16,9 +20,30 @@ $app->group('/login', function (RouteCollectorProxy $group) {
 });
 
 $app->group('/usuario', function (RouteCollectorProxy $group) {
-    $group->get('/lista', User::class . ':lista')->add(Middleware::authentication());
-    $group->get('/cadastro', User::class . ':cadastro')->add(Middleware::authentication());
+    $group->get('/lista', User::class . ':lista');
+    $group->get('/cadastro', User::class . ':cadastro');
     $group->post('/insert', User::class . ':insert');
     $group->post('/delete', User::class . ':delete');
     $group->post('/listauser', User::class . ':listauser');
+});
+$app->group('/cliente', function (RouteCollectorProxy $group) {
+    $group->get('/lista', Cliente::class . ':lista');
+    $group->get('/cadastro', Cliente::class . ':cadastro');
+    $group->post('/insert', Cliente::class . ':insert');
+    $group->post('/delete', Cliente::class . ':delete');
+    $group->post('/listacliente', Cliente::class . ':listacliente');
+});
+$app->group('/fornecedor', function (RouteCollectorProxy $group) {
+    $group->get('/lista', Fornecedor::class . ':lista');
+    $group->get('/cadastro', Fornecedor::class . ':cadastro');
+    $group->post('/insert', Fornecedor::class . ':insert');
+    $group->post('/delete', Fornecedor::class . ':delete');
+    $group->post('/listafornecedor', Fornecedor::class . ':listafornecedor');
+});
+$app->group('/empresa', function (RouteCollectorProxy $group) {
+    $group->get('/lista', Empresa::class . ':lista');
+    $group->get('/cadastro', Empresa::class . ':cadastro');
+    $group->post('/insert', Empresa::class . ':insert');
+    $group->post('/delete', Empresa::class . ':delete');
+    $group->post('/listaempresa', Empresa::class . ':listaempresa');
 });
