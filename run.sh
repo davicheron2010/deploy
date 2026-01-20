24020334b24cb570fa530de5d6d88a459905c5aa
davicheron2010 <davilvl12010@gmail.com>
2026-01-19 21:25:25 -0400

    arrumando

--- run.sh
+++ run.sh
@@ -41,43 +41,148 @@ configurar_postgresql() {
 
 }
 
-criar_tabelas() {
+criar_tabelas() 
 
     sudo -u postgres psql -d "$NOME_BANCO" -c "
-   CREATE TABLE IF NOT EXISTS usuario (
-        id bigserial PRIMARY KEY,
-        nome text,
-        sobrenome text,
-        cpf text,
-        rg text,
-        data_nascimento date,
-        senha text,
-        ativo boolean DEFAULT false,
-        administrador boolean DEFAULT false,
+    CREATE TABLE usuario (
+        id BIGSERIAL PRIMARY KEY,
+        nome TEXT,
+        sobrenome TEXT,
+        cpf TEXT,
+        rg TEXT,
+        senha TEXT,
+        ativo BOOLEAN DEFAULT FALSE,
+        administrador BOOLEAN DEFAULT FALSE,
         codigo_verificacao text,
-        data_cadastro timestamp DEFAULT CURRENT_TIMESTAMP,
-        data_alteracao timestamp DEFAULT CURRENT_TIMESTAMP
+        data_cadastro TIMESTAMP default CURRENT_TIMESTAMP ,
+        data_alteracao TIMESTAMP default CURRENT_TIMESTAMP
+    );
+    "
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE TABLE cliente (
+        id BIGSERIAL PRIMARY KEY,
+        nome_fantasia TEXT,
+        sobrenome_razao TEXT,
+        cpf_cnpj TEXT,
+        rg_ie TEXT,
+        senha TEXT,
+        ativo BOOLEAN DEFAULT FALSE,
+        codigo_verificacao text,
+        data_cadastro TIMESTAMP default CURRENT_TIMESTAMP ,
+        data_alteracao TIMESTAMP default CURRENT_TIMESTAMP
+    );
+    "
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE TABLE fornecedor (
+        id BIGSERIAL PRIMARY KEY,
+        nome_fantasia TEXT,
+        sobrenome_razao TEXT,
+        cpf_cnpj TEXT,
+        rg_ie TEXT,
+        senha TEXT,
+        ativo BOOLEAN DEFAULT FALSE,
+        codigo_verificacao text,
+        data_cadastro TIMESTAMP default CURRENT_TIMESTAMP ,
+        data_alteracao TIMESTAMP default CURRENT_TIMESTAMP
+    );
+    "
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE TABLE empresa (
+        id BIGSERIAL PRIMARY KEY,
+        nome_fantasia TEXT,
+        razao_social TEXT,
+        cnpj TEXT,
+        ie TEXT,
+        senha TEXT,
+        ativo BOOLEAN DEFAULT FALSE,
+        codigo_verificacao text,
+        data_cadastro TIMESTAMP default CURRENT_TIMESTAMP ,
+        data_alteracao TIMESTAMP default CURRENT_TIMESTAMP
+    );
+    "
+
+
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE TABLE contato (
+        id BIGSERIAL PRIMARY KEY,
+        id_usuario BIGINT REFERENCES usuario(id) on update cascade on delete cascade,
+        id_cliente BIGINT REFERENCES cliente(id) on update cascade on delete cascade,
+        id_fornecedor BIGINT REFERENCES fornecedor(id) on update cascade on delete cascade,
+        id_empresa BIGINT REFERENCES empresa(id) on update cascade on delete cascade,
+        tipo TEXT,
+        contato TEXT,
+        data_cadastro TIMESTAMP,
+        data_alteracao TIMESTAMP
     );
     "
 
     sudo -u postgres psql -d "$NOME_BANCO" -c "
-    CREATE TABLE IF NOT EXISTS contato (
-        id bigserial PRIMARY KEY,
-        id_usuario bigint,
-        tipo text,
-        contato text,
-        data_cadastro timestamp,
-        data_alteracao timestamp,
-        CONSTRAINT contato_id_usuario_fkey FOREIGN KEY (id_usuario)
-            REFERENCES public.usuario (id)
-            ON UPDATE NO ACTION
-            ON DELETE NO ACTION
-    );
-    "
+    CREATE OR REPLACE VIEW vw_cliente_contatos AS
+    SELECT 
+        cl.id,
+        cl.nome_fantasia,
+        cl.sobrenome_razao,
+        cl.cpf_cnpj,
+        cl.rg_ie,
+        cl.senha,
+        cl.ativo,
+        cl.codigo_verificacao,
+        MAX(CASE WHEN c.tipo = 'email' THEN c.contato END) AS email,
+        MAX(CASE WHEN c.tipo = 'celular' THEN c.contato END) AS celular,
+        MAX(CASE WHEN c.tipo = 'whatsapp' THEN c.contato END) AS whatsapp,
+        cl.data_cadastro,
+        cl.data_alteracao
+    FROM cliente cl
+    LEFT JOIN contato c ON c.id_cliente = cl.id
+    GROUP BY cl.id, cl.nome_fantasia, cl.sobrenome_razao, cl.cpf_cnpj, cl.rg_ie, cl.data_cadastro, cl.data_alteracao;
 
+    "
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE OR REPLACE VIEW vw_fornecedor_contatos AS
+    SELECT 
+        f.id,
+        f.nome_fantasia,
+        f.sobrenome_razao,
+        f.cpf_cnpj,
+        f.rg_ie,
+        f.senha,
+        f.ativo,
+        f.codigo_verificacao,
+        MAX(CASE WHEN c.tipo = 'email' THEN c.contato END) AS email,
+        MAX(CASE WHEN c.tipo = 'celular' THEN c.contato END) AS celular,
+        MAX(CASE WHEN c.tipo = 'whatsapp' THEN c.contato END) AS whatsapp,
+        f.data_cadastro,
+        f.data_alteracao
+    FROM fornecedor f
+    LEFT JOIN contato c ON c.id_fornecedor = f.id
+    GROUP BY f.id, f.nome_fantasia, f.sobrenome_razao, f.cpf_cnpj, f.rg_ie, f.data_cadastro, f.data_alteracao;
+
+    "
+    sudo -u postgres psql -d "$NOME_BANCO" -c "
+    CREATE OR REPLACE VIEW vw_empresa_contatos AS
+    SELECT 
+        e.id,
+        e.nome_fantasia,
+        e.razao_social,
+        e.cnpj,
+        e.ie,
+        e.senha,
+        e.ativo,
+        e.codigo_verificacao,
+        MAX(CASE WHEN c.tipo = 'email' THEN c.contato END) AS email,
+        MAX(CASE WHEN c.tipo = 'celular' THEN c.contato END) AS celular,
+        MAX(CASE WHEN c.tipo = 'whatsapp' THEN c.contato END) AS whatsapp,
+        e.data_cadastro,
+        e.data_alteracao
+    FROM empresa e
+    LEFT JOIN contato c ON c.id_empresa = e.id
+    GROUP BY e.id, e.nome_fantasia, e.razao_social, e.cnpj, e.ie, e.data_cadastro, e.data_alteracao;
+
+    "
     sudo -u postgres psql -d "$NOME_BANCO" -c "
     CREATE OR REPLACE VIEW vw_usuario_contatos AS
-    SELECT u.id,
+    SELECT 
+        u.id,
         u.nome,
         u.sobrenome,
         u.cpf,
@@ -86,16 +191,15 @@ criar_tabelas() {
         u.ativo,
         u.administrador,
         u.codigo_verificacao,
-        MAX(CASE WHEN c.tipo = 'email' THEN c.contato ELSE NULL END) AS email,
-        MAX(CASE WHEN c.tipo = 'celular' THEN c.contato ELSE NULL END) AS celular,
-        MAX(CASE WHEN c.tipo = 'whatsapp' THEN c.contato ELSE NULL END) AS whatsapp,
+        MAX(CASE WHEN c.tipo = 'email' THEN c.contato END) AS email,
+        MAX(CASE WHEN c.tipo = 'celular' THEN c.contato END) AS celular,
+        MAX(CASE WHEN c.tipo = 'whatsapp' THEN c.contato END) AS whatsapp,
         u.data_cadastro,
         u.data_alteracao
     FROM usuario u
     LEFT JOIN contato c ON c.id_usuario = u.id
     GROUP BY u.id, u.nome, u.sobrenome, u.cpf, u.rg, u.data_cadastro, u.data_alteracao;
     "
-}
 
 configurar_postgresql
 criar_tabelas
